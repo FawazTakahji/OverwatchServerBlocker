53e8e2bdf57c315174e4ac4478d60bbfcfc12a79
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2025-12-25 13:51:02 +0300

    Add Linux platform support
    Introduced OverwatchServerBlocker.Linux project with initial Program.cs and project file. Added FirewallManagerStub to Core for unsupported OS handling. Updated solution and related project files to support multi-targeting and Linux integration.

---
+++ src/OverwatchServerBlocker.Core/Stubs/FirewallManagerStub.cs
@@ -0,0 +1,21 @@
+﻿using OverwatchServerBlocker.Core.Services;
+
+namespace OverwatchServerBlocker.Core.Stubs;
+
+public class FirewallManagerStub : IFirewallManager
+{
+    public IFirewallRule[] GetRules()
+    {
+        throw new NotImplementedException("Current OS is not supported");
+    }
+
+    public IFirewallRule AddRule(IFirewallRule rule)
+    {
+        throw new NotImplementedException("Current OS is not supported");
+    }
+
+    public void RemoveRule(IFirewallRule rule)
+    {
+        throw new NotImplementedException("Current OS is not supported");
+    }
+}
